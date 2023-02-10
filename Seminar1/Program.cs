a30b2a08aa711f37ee4236dcdf3309a948731ba6
Modena <Modena2311@gmail.com>
2023-02-10 21:42:25 +0300

    Проработка заданий разного типа

---
+++ Seminar1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1. Число в квадрате (шифт альт ф - для выравнвания)
+
+// Console.WriteLine("imput number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Quad of {num} is {quad}");
+
+// Задание 2 Два числа и проверяем, является ли первое число квадратом второго
+
+// Console.WriteLine("imput first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("imput second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if(quad == num1){
+//     Console.WriteLine("Firs number is quad of second number");
+// }
+// else{
+//     Console.WriteLine("Firs number is not quad of second number");
+// }
+
+// Задание 3 Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+//  Console.WriteLine("imput number");
+//  int num = Convert.ToInt32(Console.ReadLine());
+
+//  int current = num * (-1); // current - счетчик
+
+//  while(current <= num)
+//  {
+//      Console.Write(current + " ");
+//      //current ++;    
+// }
+
+//Задание 4 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+    
+    // 456 -> 6
+    
+    // 782 -> 2
+    
+    // 918 -> 8
+
+//  Console.WriteLine("imput tree-digit number");
+//  int num = Convert.ToInt32(Console.ReadLine());// zaprasivaem
+
+//  int result = num % 10;
+
+//  Console.WriteLine( $"last digit of {num} is {result}");// $ позволяет использова {} и выводить их
