b11d1edebf697f16804212f53272b5be8ec5bb51
Modena <Modena2311@gmail.com>
2023-02-15 18:55:00 +0300

    gotovyaomashka

--- homework/Program.cs
+++ homework/Program.cs
@@ -4,11 +4,62 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+
+// int max = 0;
+// int min = 0;
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber > secondNumber)
+// {
+//     max = firstNumber;
+//     min = secondNumber;
+// }
+// else
+// {
+//     max = secondNumber;
+//     min = firstNumber;
+// }
+// Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+
+
+
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+// int max = 0;
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber > max)
+// {
+//     max = firstNumber;
+// }
+// if(secondNumber > max)
+// {
+//     max = secondNumber;
+// }
+// if(thirdNumber > max)
+// {
+//     max = thirdNumber;
+// }
+
+// Console.WriteLine("max = " + max);
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
@@ -16,31 +67,35 @@
 // -3 -> нет
 // 7 -> нет
 
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int remDiv = number % 2;
+
+// if(remDiv == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// Решение задачи 2
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("FirstNumber  ");
-int FirstNumber = Convert.ToInt32(Console.Read());
-Console.WriteLine("SecondNumber  ");
-int SecondNumber = Convert.ToInt32(Console.Read());
+// int evenNumber = 2;
 
-int max = 0;
-int min = 0;
+// if(number > 1)
+// {
+//     while(evenNumber <= number)
+//     {
+//         Console.Write(evenNumber + " ");
+//         evenNumber = evenNumber + 2;
+//     }
+// }
 
-if (FirstNumber > SecondNumber)
- {
-    max = FirstNumber;
-    min = SecondNumber;
- }
-
- else
-
- {
-    max = SecondNumber;
-    min = FirstNumber;
- }
- 
